e83a893b23bd63775f7fede22810d70cb960a2c5
Ekemini Samuel <favourekemini@yahoo.com>
2022-09-08 21:12:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void) {
-	puts("Programming is like building a multilingual puzzle\n");
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
