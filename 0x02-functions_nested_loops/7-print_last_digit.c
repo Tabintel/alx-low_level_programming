86c2bfb3d87bea1610109303e71c6e5299d0cebd
Ekemini <80429893+Tabintel@users.noreply.github.com>
2022-09-18 12:47:03 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,6 @@
  */
 int print_last_digit(int r)
 {
-<<<<<<< HEAD
 	int n;
 
 	if (r < 0)
@@ -20,11 +19,4 @@ int print_last_digit(int r)
 
 	_putchar((n % 10) + '0');
 	return (n % 10);
-=======
-	int last_digit = n % 10;
-	if (last_digit < 0)
-	last_digit *= -1 '\n';
-	_putchar(last_digit + '0');
-	return (last_digit);
->>>>>>> 2a0a6b85926b4bb18c136c3997ff02b67e0efa27
 }
